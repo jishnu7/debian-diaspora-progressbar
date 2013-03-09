9d272bf2ac13ed23d79e843a76dd2c03313c1518
Jishnu Mohan <jishnu7@gmail.com>
2013-03-09 16:27:41 +0530

    pep8 cleaning

--- process_dot.py
+++ process_dot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-import re, sys
-from jinja2 import Template, Environment, FileSystemLoader
+import re
+import sys
+from jinja2 import Environment, FileSystemLoader
 
 """
     Process dot file and categorize gems.
@@ -10,18 +11,17 @@ from jinja2 import Template, Environment, FileSystemLoader
 
 dot_file_name = sys.argv[1]
 dot_file = open(dot_file_name)
-gem_list = set() # empty set
+gem_list = set()  # empty set
 #color of node and status
-colors = {\
-'black': 'unpackaged',\
-'orange': "ITP",\
-'purple': "RFP",\
-'yellow': "new",\
-'green': "present"\
-}
+colors = {'black': 'unpackaged',
+          'orange': "ITP",
+          'purple': "RFP",
+          'yellow': "new",
+          'green': "present"
+          }
 
 #add anything is quotes to set
-matches=re.findall(r'\"(.+?)\"',dot_file.read())
+matches = re.findall(r'\"(.+?)\"', dot_file.read())
 for match in matches:
     gem_list.add(match)
 
@@ -29,22 +29,22 @@ for match in matches:
 for color in colors:
     try:
         gem_list.remove(color)
-    except KeyError: # if specific color is not defined, skip
+    except KeyError:  # if specific color is not defined, skip
         pass
 
-dot_file.seek(0) # reset
+dot_file.seek(0)  # reset
 
-#regular expression to match 
+#regular expression to match
 #"gem" [color="color"];
-re1='.*?'	# Non-greedy match on filler
-re2='((?:[a-z][a-z0-9_\-]+))'	# Word 1
-re3='.*?'	# Non-greedy match on filler
-re4='(color)'	# Variable Name 1
-re5='.*?'	# Non-greedy match on filler
-re6='((?:[a-z][a-z]+))'	# Word 2
+re1 = '.*?'	 # Non-greedy match on filler
+re2 = '((?:[a-z][a-z0-9_\-]+))'	 # Word 1
+re3 = '.*?'	 # Non-greedy match on filler
+re4 = '(color)'	 # Variable Name 1
+re5 = '.*?'	 # Non-greedy match on filler
+re6 = '((?:[a-z][a-z]+))'  # Word 2
 regexp = re1+re2+re3+re4+re5+re6
 
-matches=re.findall(regexp,dot_file.read())
+matches = re.findall(regexp, dot_file.read())
 
 gem_status = {}
 
